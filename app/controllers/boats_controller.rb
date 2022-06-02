80befbc0c877c940862256bc294b0d222f934d41
Cal Norman <calnorman1@gmail.com>
2022-06-02 16:34:58 +0100

    added more style to boat showpage cards

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,7 +1,6 @@
 class BoatsController < ApplicationController
-
-before_action :find_by_id, only: [:show, :edit, :update, :destroy]
-skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :find_by_id, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def new
     @boat = Boat.new
@@ -34,7 +33,7 @@ skip_before_action :authenticate_user!, only: %i[index show]
   def destroy
     # @boat = Boat.find(params[:id])
     @boat.destroy
-    redirect_to boats_path
+    redirect_to boats_path, status: :see_other
   end
 
   private
