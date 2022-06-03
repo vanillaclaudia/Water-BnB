1596570de19799bd7b50618c2f6f60d1a2f5af9a
Alex <amstraughan@gmail.com>
2022-06-03 11:31:46 +0100

    so close yet so far

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,6 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_one_attached :photo
 
   validates :boat_type, :total_occupancy, :price, presence: true
